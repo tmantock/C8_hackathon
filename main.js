5fc81697df20b2425249f8f395c2e1a0340129e7
Thomas Case <thomas.e.case@gmail.com>
2016-05-12 11:48:24 -0700

    basic twitter API call is successful

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 /**
  * Created by Mantock on 5/12/16.
  */
+
+// ------ Twitter API call
+$(document).ready(function(){
+    apis.twitter.getData('Beyonce concert',
+        function(success, response){
+            var my_tweets = response.tweets.statuses;
+            for(var i = 0 ; i < response.tweets.statuses.length; i++){
+                console.log(my_tweets[i].created_at);
+                console.log(my_tweets[i].text);
+            }
+            console.log(response);
+        }
+    );
+});
