337486d9b00f1c794653311e8c6a0fae275e65b4
Peter Whang <Peter Whang>
2016-05-12 12:12:10 -0700

    youtube api

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 /**
  * Created by Mantock on 5/12/16.
  */
+var artist1 = [];
+var nickleback = [];
+
+$(document).ready(function() {
+    apis.youtube.getData('beyonce', 5, function(success, response){
+        console.log(success);
+        if(success){
+            for(var x = 0; x < response.video.length ; x++){
+                artist1.push(response.video[x]);
+                console.log('response', response);
+                
+            }
+            console.log(artist1)
+        }
+    });
+    
+});
