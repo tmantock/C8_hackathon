5c8e30aaa6f4fa883f9d2640a52ef2f5fe3f113f
Tevin Mantock <tmantock@gmail.com>
2016-05-13 12:49:23 -0700

    added page scroll down for main return

--- main.js
+++ main.js
@@ -62,8 +62,9 @@ function page_scroll () {
     var xposition = $('#home_page').position().top;
     var xHeight = $('#home_page').height();
 
+
     $('.drop_animate').toggle('slow');
-    $('#main_page').animate({top:(-1*xHeight) + xposition + 'px'},900, function () {
+    $('#main_page').animate({top:(-1*xHeight) + 'px'},900, function () {
         apis.twitter.getData('beyonce I am... tour',
             function (success, response) {
                 var my_tweets = response.tweets.statuses;
@@ -96,7 +97,6 @@ function process_twitter_api(response) {
 }
 
 function twitterList (tweet_object_array) {
-    //var firstDiv = $('<div>').addClass('twitter_card').attr('data',1);
     var temp_div = $('<div>').addClass('twitter_card');
     var temp_text = $('<div>').addClass('tweet_text');
     var temp_pic = $('<img>').addClass('tweet_user_pic');
@@ -111,6 +111,7 @@ function twitterList (tweet_object_array) {
     temp_div.append(temp_pic, temp_user_name, temp_text);
     $('.twitter_feed').append(temp_div);
     var counter = 1;
+
     for(i=1; i<tweet_object_array.length; i++) { //array should start at 1
         temp_div = $('<div>').addClass('twitter_card');
         temp_text = $('<div>').addClass('tweet_text');
@@ -125,7 +126,7 @@ function twitterList (tweet_object_array) {
         temp_div.append(temp_pic, temp_user_name, temp_text);
         var lastPosition = $('.twitter_feed .twitter_card:first-child').position().top;
         var lastHeight = $('.twitter_feed .twitter_card:first-child').height();
-        $('.twitter_feed').append(temp_div.attr('data-count',counter));
+        $('.twitter_feed').append(temp_div.attr('data-count',counter).css({top: lastPosition + 15}));
         $('.twitter_feed div:last-child').animate({top:(lastHeight * counter) + (15 * counter++) + 'px'}, 1000);
     }
 }
@@ -207,7 +208,7 @@ function speaker () {
     $('.speaker').append(speaker_div.css('top',speaker_position));
     var second_speaker_position;
     var large_speaker;
-    $('.speaker_animate').animate({top: speaker_position + 150 + 'px'},1000);
+    $('.speaker_animate').animate({top: speaker_position + 250 + 'px'},1000);
     setTimeout(function () {
         second_speaker_position = $('.speaker_animate').position().top;
         large_speaker = $('<div>').addClass('speaker_hole speaker_grow');
@@ -215,9 +216,11 @@ function speaker () {
         $('.speaker_grow').animate({top: '-=25', height: '+=50', width: '+=50'},500);
     },1000);
 }
+
 var ra = 0;
 var rd = 0;
 var et = 0;
+
 function ramrod () {
     var ram = 'am';
     var rod = 'od';
@@ -246,5 +249,10 @@ function ramrod_leave () {
     ra = 0;
     rd = 0;
     et = 0;
+}
+
+function home_slide () {
+    var mainHeight = $('#main_page').height();
+    $('#main_page').animate({top:mainHeight + 'px'},1500);
 }
 
